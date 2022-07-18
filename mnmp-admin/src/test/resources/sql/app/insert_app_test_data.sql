ba6d607447e8e72e3a8d052e0c29adb61beabff2
harry <zh_harry@163.com>
2022-07-18 11:43:43 +0800

    modify connection url

--- mnmp-admin/src/test/resources/sql/app/insert_app_test_data.sql
+++ mnmp-admin/src/test/resources/sql/app/insert_app_test_data.sql
@@ -15,56 +15,6 @@
  * limitations under the License.
  */
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 INSERT  INTO `notice_app`(`id`,`code`,`name`,`leader`,`team_members`,`remarks`,`audit_status`,`enabled`,`create_time`,`update_time`) VALUES (1,'tctm','童程童美','housiyang,','tianliang,zhoushibo','童程童美1',0,0,NOW(),NOW());
 INSERT  INTO `notice_app`(`id`,`code`,`name`,`leader`,`team_members`,`remarks`,`audit_status`,`enabled`,`create_time`,`update_time`) VALUES (2,'mtdm','萌兔动漫','tanglili,','tanglili,liujixiang','萌兔',0,1,NOW(),NOW());
 INSERT  INTO `notice_app`(`id`,`code`,`name`,`leader`,`team_members`,`remarks`,`audit_status`,`enabled`,`create_time`,`update_time`) VALUES (20,'sso-corp','员工登录','liujixiang','liujixiang,liuhuan3','员工统一登录',0,1,NOW(),NOW());
