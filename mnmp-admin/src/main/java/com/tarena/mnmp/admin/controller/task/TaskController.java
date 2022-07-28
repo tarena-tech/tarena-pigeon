9d4f3fa72038817a451c098bf1e41e1812cef205
yangbd <yangbd@tedu.cn>
2022-07-28 15:46:28 +0800

    style

--- mnmp-admin/src/main/java/com/tarena/mnmp/admin/controller/task/TaskController.java
+++ mnmp-admin/src/main/java/com/tarena/mnmp/admin/controller/task/TaskController.java
@@ -49,7 +49,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
