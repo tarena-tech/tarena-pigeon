6cd8d2b9c672f4ac11a13daf09c4b0b2270fea3f
xiaoxuwei <xiaoxw861319@163.com>
2022-08-10 14:46:04 +0800

    modify passport

--- mnmp-passport/src/main/java/com/tarena/mnmp/passport/controller/TestAuthorizationController.java
+++ mnmp-passport/src/main/java/com/tarena/mnmp/passport/controller/TestAuthorizationController.java
@@ -22,9 +22,11 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 //@ApiIgnore
 @RestController
+@ApiIgnore
 @Api(tags = "授权测试")
 public class TestAuthorizationController {
     @RequestMapping("/authorize/test1")
