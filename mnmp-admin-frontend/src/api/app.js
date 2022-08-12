dda3f6fcd91a11ce0c9076f3e1c011bc48c0cc5f
yangbd <yangbd@tedu.cn>
2022-08-12 11:41:58 +0800

    修改地址

--- mnmp-admin-frontend/src/api/app.js
+++ mnmp-admin-frontend/src/api/app.js
@@ -4,6 +4,6 @@ import {
 
 export const queryPage = (data) => get('/admin/app/query/page', data)
 export const changeEnable = (params) => post('/app/change/enable/status?id=' + params, null, null)
-export const audit = (data) => post('/app/audit', data, null)
-export const save = (data) => post('/app/save', data, null)
-export const queryAppList = (params) => get('/app/query/list', params, null)
+export const audit = (data) => post('/admin/app/audit', data, null)
+export const save = (data) => post('/admin/app/save', data, null)
+export const queryAppList = (params) => get('/admin/app/query/list', params, null)
