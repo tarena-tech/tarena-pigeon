da39c13c0a59036adb22a9827fe84ac5a59536cb
杨帮东 <397827222@qq.com>
2022-08-18 10:11:59 +0800

    vue 前端持久化

--- mnmp-admin-frontend/src/store/index.js
+++ mnmp-admin-frontend/src/store/index.js
@@ -4,8 +4,14 @@ import getters from './getters'
 import app from './modules/app'
 import settings from './modules/settings'
 import user from './modules/user'
+import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  modules: ['user']
+})
 
 const store = new Vuex.Store({
   modules: {
@@ -13,7 +19,8 @@ const store = new Vuex.Store({
     settings,
     user
   },
-  getters
+  getters,
+  plugins: [vuexLocal.plugin]
 })
 
 export default store
