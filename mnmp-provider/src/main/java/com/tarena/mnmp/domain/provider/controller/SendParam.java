90c12bac84b30cd4b662d4fcdba67d841d4b4d84
xiaoxuwei <xiaoxw861319@163.com>
2022-10-10 11:47:18 +0800

    add defaultSendParam

--- mnmp-provider/src/main/java/com/tarena/mnmp/domain/provider/controller/SendParam.java
+++ mnmp-provider/src/main/java/com/tarena/mnmp/domain/provider/controller/SendParam.java
@@ -17,14 +17,21 @@
 package com.tarena.mnmp.domain.provider.controller;
 
 import com.tarena.mnmp.api.TargetDTO;
+import com.tarena.mnmp.domain.provider.factory.SendConstants;
+import com.tarena.mnmp.domain.provider.factory.SendParamFactory;
 import com.tarena.mnmp.enums.NoticeType;
 import com.tarena.mnmp.enums.SendType;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import lombok.Data;
 
 @Data
 public class SendParam {
-
 
     public SendParam() {
         this.mock = 0;
@@ -42,4 +49,32 @@ public class SendParam {
     private String providerCode;
     private Integer mock;
     private Long taskId;
+
+    public static class DefaultSendParam {
+        public SendParam defaultSendParam(String code, String phone) {
+            Properties properties = SendParamFactory.getProperties();
+            //发送验证码到手机
+            SendParam sendParam = new SendParam();
+            sendParam.setSendType(SendType.IMMEDIATELY);
+            sendParam.setNoticeType(NoticeType.SMS);
+            sendParam.setAppCode(properties.getProperty(SendConstants.APP_CODE));
+            sendParam.setSignCode(properties.getProperty(SendConstants.SIGN_CODE));
+            sendParam.setSignName("华为云短信测试");
+            sendParam.setTemplateCode(properties.getProperty(SendConstants.TEMP_CODE));
+            sendParam.setTemplateContent(properties.getProperty(SendConstants.TEMP_CONTENT));
+            sendParam.setMock(0);
+            sendParam.setProviderCode(properties.getProperty(SendConstants.PROV_CODE));
+            Map<String, Object> params = new HashMap<>();
+            params.put("msg", code);
+            //sendParam.setTaskId(-19999L);
+            TargetDTO targetDTO = new TargetDTO(phone, params);
+            List<TargetDTO> targetDTOS = new ArrayList<>();
+            targetDTOS.add(targetDTO);
+            sendParam.setTargets(targetDTOS);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(properties.getProperty(SendConstants.PATTERN));
+            String triggertime = LocalDateTime.now().format(formatter).toString();
+            sendParam.setTriggerTime(triggertime);
+            return sendParam;
+        }
+    }
 }
